2503087aecdf181eaa5510a2f97e7b7f4f690fe5
snehal211998 <49609821+snehal211998@users.noreply.github.com>
2021-06-17 23:10:37 +0530

    Added function to query with params, insert and update

--- src/main/java/com/wit/user/manager/dao/CapitalDaoSupport.java
+++ src/main/java/com/wit/user/manager/dao/CapitalDaoSupport.java
@@ -5,6 +5,8 @@ import com.wit.user.manager.exception.ServiceErrorTypes;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.support.JdbcDaoSupport;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 
 import javax.sql.DataSource;
 import java.util.List;
@@ -32,4 +34,26 @@ public class CapitalDaoSupport extends JdbcDaoSupport {
             throw new ApiException(e, ServiceErrorTypes.DATA_NOT_FOUND);
         }
     }
+
+    public <T> List<T> queryForParams(String query, Object[] params, RowMapper<T> mapper) throws ApiException {
+        try {
+            return getJdbcTemplate().query(query, params, mapper);
+
+        } catch (DataAccessException e) {
+            throw new ApiException(e, ServiceErrorTypes.DATA_NOT_FOUND);
+        }
+    }
+
+    public int update(String query, Object[] params)
+    {
+        return getJdbcTemplate().update(query, params);
+    }
+
+    public long insert(String query, Object[] params)
+    {
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        getJdbcTemplate().update(query, params, keyHolder);
+        return (long) keyHolder.getKey();
+    }
+
 }
