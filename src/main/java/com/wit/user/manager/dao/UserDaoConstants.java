54e03e4fcf6789652f692d69c02b4f666b0dd60d
snehal211998 <49609821+snehal211998@users.noreply.github.com>
2021-06-18 10:40:07 +0530

    Updated SQL's as per new column names

--- src/main/java/com/wit/user/manager/dao/UserDaoConstants.java
+++ src/main/java/com/wit/user/manager/dao/UserDaoConstants.java
@@ -4,13 +4,13 @@ import java.util.Date;
 
 public class UserDaoConstants {
 
-    public static final String FETCH_PRODUCTS_QUERY = "select prod_id, prod_name, qty from product";
+    public static final String FETCH_PRODUCTS_QUERY = "select product_id, product_name, quantity from product";
 
-    public static final String FETCH_ALL_PRODUCTDETAILS = "SELECT productName,expiryDate,manufacturerName,quantity,productId,image,productType,isWorking FROM ProductDetails";
-    public static final String FETCH_PRODUCTDETAILS_BYNAME= "SELECT productName,expiryDate,manufacturerName,quantity,productId,image,productType,isWorking FROM ProductDetails where productName = ?";
-    public static final String FETCH_PRODUCTDETAILS_BYTYPE = "SELECT productName,expiryDate,manufacturerName,quantity,productId,image,productType,isWorking FROM ProductDetails where type = ?";
+    public static final String FETCH_ALL_PRODUCTDETAILS = "SELECT product_name,expiry_date,maf_name,quantity,product_id,image,type,is_working,category FROM product";
+    public static final String FETCH_PRODUCTDETAILS_BYNAME= "SELECT product_name,expiry_date,maf_name,quantity,product_id,image,type,is_working,category FROM product where productName = ?";
+    public static final String FETCH_PRODUCTDETAILS_BYTYPE = "SELECT product_name,expiry_date,maf_name,quantity,product_id,image,type,is_working,category FROM product where type = ?";
 
-    public static final String FETCH_PRODUCTDETAILS_BYCATEGORY="SELECT productName, expiryDate, manufacturerName, quantity, productType, category FROM DonatedProducts WHERE category=?";
+    public static final String FETCH_PRODUCTDETAILS_BYCATEGORY="SELECT product_name,expiry_date,maf_name,quantity,product_id,image,type,is_working,category FROM product WHERE category=?";
 
     private UserDaoConstants() {
     }
--- src/main/java/com/wit/user/manager/mapper/ProductDetailsRowMapper.java
+++ src/main/java/com/wit/user/manager/mapper/ProductDetailsRowMapper.java
@@ -1,11 +1,11 @@
 package com.wit.user.manager.mapper;
 
-        import com.wit.user.manager.model.Product;
-        import com.wit.user.manager.model.ProductDetails;
-        import org.springframework.jdbc.core.RowMapper;
+import com.wit.user.manager.model.Product;
+import com.wit.user.manager.model.ProductDetails;
+import org.springframework.jdbc.core.RowMapper;
 
-        import java.sql.ResultSet;
-        import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class ProductDetailsRowMapper implements RowMapper<ProductDetails> {
 
@@ -13,15 +13,15 @@ public class ProductDetailsRowMapper implements RowMapper<ProductDetails> {
     public ProductDetails mapRow(ResultSet rs, int rowNum) throws SQLException {
         ProductDetails product = new ProductDetails();
 
-        product.setProductId(rs.getInt("productId"));
-        product.setProductName(rs.getString("productName"));
+        product.setProductId(rs.getInt("product_id"));
+        product.setProductName(rs.getString("product_name"));
         product.setProductType(rs.getString("type"));
 
-        product.setExpiryDate(rs.getDate("expiryDate"));
-        product.setManufacturerName(rs.getString("manufacturerName"));
+        product.setExpiryDate(rs.getDate("expiry_date"));
+        product.setManufacturerName(rs.getString("maf_name"));
         product.setQuantity(rs.getInt("quantity"));
         product.setImage(rs.getString("image"));
-        product.setIsWorking(rs.getBoolean("isWorking"));
+        product.setIsWorking(rs.getBoolean("is_working"));
         product.setCategory(rs.getString("category"));
         return product;
     }
